0f2f12ce5e841c10793aab01cbb89329f60aa3a0
RhuanAbbud <rhuan.abbud@hotmail.com>
2024-07-01 00:57:30 -0300

    REFORMULACAO COMPLETA DA API

--- src/main/java/br/com/bibliaapitranslater/repository/CapituloRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.bibliaapitranslater.repository;
-
-import br.com.bibliaapitranslater.model.Capitulo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CapituloRepository extends JpaRepository<Capitulo, Long> {
-}
--- src/main/java/br/com/bibliaapitranslater/repository/VersiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.bibliaapitranslater.repository;
-
-import br.com.bibliaapitranslater.model.Versiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VersiculoRepository extends JpaRepository<Versiculo, Long> {
-}
