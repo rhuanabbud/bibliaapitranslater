678fa450850432eb2b7ab5aadd30d56f108dfb25
RhuanAbbud <rhuan.abbud@hotmail.com>
2024-07-01 22:17:23 -0300

    remove nao utilizado

--- src/main/java/br/com/bibliaapitranslater/repository/BibliaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.bibliaapitranslater.repository;
-
-import br.com.bibliaapitranslater.model.Biblia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BibliaRepository extends JpaRepository<Biblia, Long> {
-}
